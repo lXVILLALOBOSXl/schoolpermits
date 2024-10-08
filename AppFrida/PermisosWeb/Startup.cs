28b49ca5128addad60037f7c7775eaf060a1ff1e
Luis Villalobos <ivillalobosrivera@yahoo.com>
2024-10-08 15:30:00 -0600

    For local with non SQL Server managment Studio

--- AppFrida/PermisosWeb/Startup.cs
+++ AppFrida/PermisosWeb/Startup.cs
@@ -1,34 +1,28 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.IO;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using PermisosEntitiesLib;
-using Microsoft.EntityFrameworkCore;
-using System.IO;
+using PermisosEntitiesLib;  // Referencia al namespace que contiene el contexto `Permisos`
 
 namespace PermisosWeb
 {
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddRazorPages();
+
             string dbPath = Path.Combine("../../DataBase", "Permisos.db");
-            services.AddDbContext<Permisos>(
-                options => options.UseSqlite($"Data Source= {dbPath}")
-
+            
+            // Usar el contexto `Permisos` que está en `PermisosEntitiesLib`
+            services.AddDbContext<Permisos>(options =>
+                options.UseSqlite($"Data Source={dbPath}")
             );
-
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
@@ -40,18 +34,13 @@ namespace PermisosWeb
                 app.UseHsts();
             }
 
-            app.UseRouting();
-            app.UseHttpsRedirection();
-            app.UseDefaultFiles();
             app.UseStaticFiles();
+            app.UseRouting();
 
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapRazorPages();
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                endpoints.MapFallbackToPage("/Index");  // Asegúrate de que la página "Index.cshtml" esté presente
             });
         }
     }
